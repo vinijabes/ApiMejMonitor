002abb525aa7f5c79288d0c03617b5236785d7fb
Vinícius Jabes <vinijabes@gmail.com>
2019-08-01 13:15:56 -0300

    Document Font

--- documents/index.js
+++ documents/index.js
@@ -11,7 +11,7 @@ return `
 
             @font-face {
                 font-family: Arial;
-                src: url('${path.resolve(__dirname+'/arial.ttf')}');
+                src: url('${path.resolve(__dirname+'/Arial.woff')}');
             }
 
             body{
